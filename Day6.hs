186b15f021ab0727acc5565f43716bd4efcb1084
Niklas Konstenius <niklas.konstenius@gmail.com>
2022-12-06 16:20:30 +0100

    day 6

--- Day6.hs
+++ Day6.hs
@@ -1,19 +1,18 @@
---
--- Ett litet skal för Advent of Code 2022
---
+import           Data.List (nub, sort)
 import           Lib
 
 main :: IO ()
 main = runSolver solve1 solve2
 
 solve1 :: String -> String
-solve1 input =
-    "Antal rader: " ++ show (length (lines input))
+solve1 = show . (+4) . length . takeWhile (not . unique) . slidingWindow 4
 
 solve2 :: String -> String
-solve2 input =
-    "<inte löst ännu...>"
+solve2 = show . (+14) . length . takeWhile (not . unique) . slidingWindow 14
 
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow n xs = take n xs : slidingWindow n (drop 1 xs)
 
-
+unique :: Ord a => [a] -> Bool
+unique xs = length xs == length (nub $ sort xs)
 
