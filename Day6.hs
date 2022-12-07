e3f3bb730961ca87d0f0fbc4442dcbcc01226aee
Niklas Konstenius <niklas.konstenius@gmail.com>
2022-12-07 08:16:42 +0100

    simplify

--- Day6.hs
+++ Day6.hs
@@ -5,15 +5,15 @@ main :: IO ()
 main = runSolver solve1 solve2
 
 solve1 :: String -> String
-solve1 = show . (+4) . length . takeWhile (not . unique) . slidingWindow 4
+solve1 = show . (+4) . length . takeWhile (not . marker) . slidingWindow 4
 
 solve2 :: String -> String
-solve2 = show . (+14) . length . takeWhile (not . unique) . slidingWindow 14
+solve2 = show . (+14) . length . takeWhile (not . marker) . slidingWindow 14
 
 slidingWindow :: Int -> [a] -> [[a]]
 slidingWindow n xs | length xs < n = []
                    | otherwise     = take n xs : slidingWindow n (drop 1 xs)
 
-unique :: Ord a => [a] -> Bool
-unique xs = length xs == length (nub $ sort xs)
+marker :: Ord a => [a] -> Bool
+marker xs = xs == nub xs
 
