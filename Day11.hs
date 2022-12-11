494d44e78b3ce4761f81eaa71b4fbdf5252e132d
konnik <niklas.konstenius@gmail.com>
2022-12-11 17:59:11 +0100

    day 11 still ugly

--- Day11.hs
+++ Day11.hs
@@ -7,21 +7,25 @@ main = runSolver solve1 solve2
 
 solve1 :: String -> String
 solve1 input =
-    show $ product $ take 2 $ reverse $ sort $ fmap count $ head $ drop 20 $ iterate (round (`div` 3)) $ parse input
-   -- show $ fmap count $ head $ drop 20 $ iterate round $ parse input
+    show $ solve 20 limitByDiv monkeys
+    where
+        limitByDiv = (`div` 3)
+        monkeys = parse input
 
 solve2 :: String -> String
 solve2 input =
-    --show $ product $ take 2 $ reverse $ sort $ fmap count $ head $ drop 1 $ iterate round $ parse input
-
-    show $ product $ take 2 $ reverse $ sort $ fmap count $ head $ drop 10000 $ iterate (round f)  $ parse input
-
+    show $ solve 10000 limitByMod monkeys
     where
-        f x = x `mod` factors
-        factors = product $ nub $ fmap divisibleBy $ parse input
+        limitByMod x = x `mod` product (nub $ divisibleBy <$> monkeys)
+        monkeys = parse input
+
+
+solve :: Int -> (Int -> Int) -> [Monkey] -> Int
+solve rounds f =
+    product . take 2 . reverse . sort . fmap count . (!! rounds) . iterate (round f)
 
 round :: (Int -> Int) -> [Monkey] -> [Monkey]
-round f ms = iterate (oneTurn f) ms !! (length ms)
+round f ms = iterate (oneTurn f) ms !! length ms
 
 
 oneTurn :: (Int -> Int) -> [Monkey] -> [Monkey]
@@ -31,19 +35,24 @@ oneTurn f (m:ms) =  (distribute thrownItems <$> ms) ++ [distribute thrownItems m
         (m', thrownItems) = inspectItems f m
 
 distribute :: [(Int, Int)] -> Monkey -> Monkey
-distribute xs m = m { items = items m ++ newItems }
+distribute thrownItems toMonkey = toMonkey { items = items toMonkey ++ newItems }
     where
-        newItems = fmap snd $ filter ((==monkeyId m ). fst) xs
+        newItems = snd <$> filter ((==monkeyId toMonkey) . fst) thrownItems
 
 inspectItems :: (Int -> Int) -> Monkey -> (Monkey, [(Int, Int)])
-inspectItems f m =  (m{ items = [], count = count m + length (items m)}, inspect f m <$> items m)
+inspectItems f m =
+            ( m { items = []
+                , count = count m + length (items m)
+                }
+            , inspectOne f m <$> items m
+            )
 
-inspect :: (Int -> Int) -> Monkey -> Int -> (Int, Int)
-inspect f m x = (toMonkey, x')
+inspectOne :: (Int -> Int) -> Monkey -> Int -> (Int, Int)
+inspectOne f m x = (toMonkey, x')
     where
         x' = f (op m x)
-        toMonkey = if test x' then trueMonkey m else falseMonkey m
-        test a = a `mod` divisibleBy m == 0
+        toMonkey = if test then trueMonkey m else falseMonkey m
+        test = x' `mod` divisibleBy m == 0
 
 
 data Monkey = Monkey
@@ -59,17 +68,18 @@ data Monkey = Monkey
 
 parse :: String -> [Monkey]
 parse input = map parseMonkey $ chunks 7 $ lines input
-
-parseMonkey :: [String] -> Monkey
-parseMonkey xs = Monkey id items op divisibleBy trueM falseM 0
     where
-        id =  read $ words (replace ':' ' ' (xs !! 0)) !! 1
-        items = fmap read $ drop 2 $ words $ replace ',' ' ' $ xs !! 1
-        op = case drop 4 $ words $ xs !! 2 of
-                ["*","old"] -> (\old -> old*old)
-                ["*",b]     -> (* read b)
-                ["+",b]     -> (+ read b)
-                _           -> error "Invalid op"
-        divisibleBy = read (words (xs !! 3) !! 3)
-        trueM = read $ words (xs !! 4) !! 5
-        falseM = read $ words (xs !! 5) !! 5
+    parseMonkey :: [String] -> Monkey
+    parseMonkey xs = Monkey
+        { monkeyId =  read $ words (replace ':' ' ' (xs !! 0)) !! 1
+        , items = fmap read $ drop 2 $ words $ replace ',' ' ' $ xs !! 1
+        , op = case drop 3 $ words $ xs !! 2 of
+                    ["old", "*", "old"] -> (\old -> old*old)
+                    ["old", "*", b    ] -> (* read b)
+                    ["old", "+", b    ] -> (+ read b)
+                    _                   -> error "Invalid op"
+        , divisibleBy = read (words (xs !! 3) !! 3)
+        , trueMonkey = read $ words (xs !! 4) !! 5
+        , falseMonkey = read $ words (xs !! 5) !! 5
+        , count = 0
+        }
